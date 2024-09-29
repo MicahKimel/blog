fa4d5e7905604c445a9f3d7e4ec002ddcd657295
MicahKimel <micah.kimel@my.wheaton.edu>
2024-09-29 13:16:08 -0400

    update packages

--- blog/Controllers/HomeController.cs
+++ blog/Controllers/HomeController.cs
@@ -22,20 +22,64 @@ public class HomeController : Controller
     {
         return View();
     }
-
-    public IActionResult Inquiry()
+    public IActionResult Gallery()
     {
-        ViewData["Name"] = "Kullu";
-        ViewData["Description"] = "Kullu";
-        ViewData["Posted"] = "False";
+        return View();
+    }
+
+    public IActionResult Inquiry(int id)
+    {
+        switch (id)
+        {
+            case 0:
+                ViewData["Name"] = "";
+                ViewData["Description"] = "";
+                ViewData["Posted"] = "";
+                break;
+            case 1:
+                ViewData["Name"] = "Kullu";
+                ViewData["Description"] = "Kullu, nestled amidst the majestic Himalayas, offers a breathtaking blend of natural beauty, cultural richness, and thrilling outdoor adventures. A Kullu travel package, inclusive of air travel, taxi services, hotel accommodations, fly fishing, and other activities, presents an unforgettable experience for travelers seeking a harmonious escape.";
+                ViewData["Posted"] = "";
+                break;
+            case 2:
+                ViewData["Name"] = "Manali";
+                ViewData["Description"] = "Manali, nestled amidst the majestic Himalayas, offers a breathtaking blend of natural beauty, cultural richness, and thrilling outdoor adventures. A Manali travel package, inclusive of air travel, taxi services, hotel accommodations, fly fishing, and other activities, presents an unforgettable experience for travelers seeking a harmonious escape";
+                ViewData["Posted"] = "";
+                break;
+            case 3:
+                ViewData["Name"] = "Kasol and Manikaran";
+                ViewData["Description"] = "Kasol and Manikaran, nestled amidst the picturesque Parvati Valley in Himachal Pradesh, offer a serene escape into the lap of nature. A travel package to these destinations, inclusive of air travel, taxi services, hotel accommodations, fly fishing, and other activities, presents an unforgettable experience for adventure seekers and nature lovers.";
+                ViewData["Posted"] = "";
+                break;
+            case 4:
+                ViewData["Name"] = "Hampta Pass";
+                ViewData["Description"] = "Hampta Pass is a challenging yet rewarding trek in the Pir Panjal Range of the Himalayas. A travel package to this destination, inclusive of air travel, taxi services, hotel accommodations, fly fishing, and other activities, offers an unforgettable experience for adventure enthusiasts seeking to conquer a high-altitude pass.";
+                ViewData["Posted"] = "";
+                break;
+            case 5:
+                ViewData["Name"] = "Spiti";
+                ViewData["Description"] = "Spiti Valley, often referred to as the 'Little Tibet of India', offers a unique and unforgettable experience for travelers seeking a blend of adventure, culture, and breathtaking landscapes. A travel package to Spiti Valley, inclusive of air travel, taxi services, hotel accommodations, fly fishing, and other activities, presents an ideal opportunity to explore this remote and mystical region.";
+                ViewData["Posted"] = "";
+                break;
+            case 6:
+                ViewData["Name"] = "Ladakh";
+                ViewData["Description"] = "Ladakh, often referred to as the 'Land of High Passes', offers a unique and unforgettable experience for adventure enthusiasts seeking to explore the remote and mystical regions of the Himalayas. A travel package to Ladakh, inclusive of air travel, taxi services, hotel accommodations, fly fishing, and other activities, presents an ideal opportunity to embark on a high-altitude adventure.";
+                ViewData["Posted"] = "";
+                break;
+            default:
+                ViewData["Name"] = "";
+                ViewData["Description"] = "";
+                ViewData["Posted"] = "";
+                break;
+        }
         return View();
     }
 
     [HttpPost]
-    public IActionResult Inquiry(string FirstName, string LastName, string Email, string Phone, string TextArea)
+    public IActionResult Inquiry(string FirstName, string LastName, string Email, string Phone, string TextArea, string Inquiry)
     {
         // Take the model and email
-        Console.WriteLine(FirstName + LastName + Email + Phone + TextArea);
+        Console.WriteLine(FirstName + LastName + Email + Phone + TextArea + Inquiry);
         ViewData["Posted"] = "True";
         return View();
     }
--- blog/obj/Debug/net8.0/blog.AssemblyInfo.cs
+++ blog/obj/Debug/net8.0/blog.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("blog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fe3f1846f71fc3c592e67c9d7bbf16bf9255817")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5de6380d907895a1d1ba1f7c8046793bcb7457d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("blog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("blog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
