45f0923bbc2f84df6c751dc5f8a0d179071173d3
MicahKimel <micah.kimel@my.wheaton.edu>
2024-10-02 18:12:49 -0400

    fix packages images

--- blog/obj/Debug/net8.0/blog.AssemblyInfo.cs
+++ blog/obj/Debug/net8.0/blog.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("blog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3143ee7c80d2a873844c8aa1100a31c71f3b2fb3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4956689caf4de33b89b6feb22948c9c2896fdfb0")]
 [assembly: System.Reflection.AssemblyProductAttribute("blog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("blog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
